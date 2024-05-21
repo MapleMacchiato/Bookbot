dc565b5975a81ee4244aa2edca5e9e8355871d1e
MapleMacchiato <gabriel.b.stein@gmail.com>
2024-05-21 10:43:22 -0400

    created get char counts

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     path_to_book = "books/frankenstein.txt"  
     book = open_book(path_to_book)
     print(count_words(book))
-       
+    print(count_letters(book))
 
 def open_book(path_to_book):
     book = ""
@@ -14,4 +14,14 @@ def open_book(path_to_book):
 def count_words(book):
     words = book.split()
     return len(words)
+
+def count_letters(book):
+    lower_case_book = book.lower()
+    char_count = {}
+    for c in lower_case_book:
+        if c in char_count:
+            char_count[c] += 1 
+        else:
+            char_count[c] = 1
+    return char_count
 main()
